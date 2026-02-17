55fc938a612058cae05931b391507fbc798e33f2
ayalalgg <ayalalgg@gmail.com>
2026-02-17 00:15:06 -0600

    fix(gtm): replace @next/third-parties with manual Script to fix build error

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,7 +15,7 @@ export const metadata: Metadata = {
   },
 };
 
-import { GoogleTagManager } from '@next/third-parties/google';
+import Script from 'next/script';
 const gtmId = process.env.NEXT_PUBLIC_GTM_ID || "GTM-K753W8TP";
 
 export default function RootLayout({
@@ -25,8 +25,17 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="es" className="scroll-smooth" suppressHydrationWarning>
-      <GoogleTagManager gtmId={gtmId} />
       <body className={cn("antialiased min-h-screen bg-background text-foreground font-sans")}>
+        {/* Google Tag Manager */}
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','${gtmId}');
+          `}
+        </Script>
         {/* Google Tag Manager (noscript) */}
         <noscript>
           <iframe
